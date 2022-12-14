723350184e5f7f22a0b2aefc475d020172205215
ynna02 <dshainanikole@gmail.com>
2022-12-14 21:25:11 +0700

    done

--- Desktop/yna/08index.php
+++ Desktop/yna/08index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/styles.css">
-    <title>paysapp</title>
+    <title>PeyysApp</title>
 </head>
 <body>
     <h1>Peys App</h1>
@@ -30,7 +30,7 @@
      ?>
   
   <div>
-  <img src="jj" style="width: <?php echo $ImageRangeSize . '%'; ?>;border-color: <?php echo $border; ?>;border-width: 4px; border-style: solid;">
+  <img src="https://scontent.fcrk1-3.fna.fbcdn.net/v/t39.30808-6/298568451_2291844207645818_3086252747431180306_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGO2JrWxKrfZ5OOg9FutNjhhauIMleYTeeFq4gyV5hN5-QoeZzIKLQ8FZm5VleEBSnYlmawL58KrgeXeVluv0Ok&_nc_ohc=W-FYH3xB25UAX_fdrlj&_nc_oc=AQktoymWnxykalJNSbvpMjbCKb6UqPzem-AKcpDEWt8Wlvcnyx6q3d0uL_aLHAfx3bY&_nc_ht=scontent.fcrk1-3.fna&oh=00_AfAKpVuSXhqLX4fFeoFl0a63Km2froW5jbbDzVoc0zEKbg&oe=6360451A " style="width: <?php echo $ImageRangeSize . '%'; ?>;border-color: <?php echo $border; ?>;border-width: 4px; border-style: solid;">
   </div>
   
    
